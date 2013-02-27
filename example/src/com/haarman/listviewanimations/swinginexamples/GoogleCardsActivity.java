54df5b2e84bd794d1c2e66050b861d0c861e3884
Niek Haarman <haarman.niek@gmail.com>
2013-02-27 23:19:28 +0100

    Added caching of bitmaps

--- example/src/com/haarman/listviewanimations/swinginexamples/GoogleCardsActivity.java
+++ example/src/com/haarman/listviewanimations/swinginexamples/GoogleCardsActivity.java
@@ -17,9 +17,14 @@ package com.haarman.listviewanimations.swinginexamples;
 
 import java.util.ArrayList;
 
+import android.annotation.SuppressLint;
 import android.app.Activity;
 import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.os.Build;
 import android.os.Bundle;
+import android.support.v4.util.LruCache;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -60,11 +65,34 @@ public class GoogleCardsActivity extends Activity {
 	private static class GoogleCardsAdapter extends ArrayAdapter<Integer> {
 
 		private Context mContext;
+		private LruCache<Integer, Bitmap> mMemoryCache;
 
 		public GoogleCardsAdapter(Context context) {
 			mContext = context;
+
+			final int maxMemory = (int) (Runtime.getRuntime().maxMemory() / 1024);
+
+			// Use 1/8th of the available memory for this memory cache.
+			final int cacheSize = maxMemory;
+			System.out.println("max: " + maxMemory);
+			mMemoryCache = new LruCache<Integer, Bitmap>(cacheSize) {
+				@SuppressLint("NewApi")
+				@Override
+				protected int sizeOf(Integer key, Bitmap bitmap) {
+					// The cache size will be measured in kilobytes rather than
+					// number of items.
+					int size;
+					if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.HONEYCOMB_MR1) {
+						size = bitmap.getByteCount() / 1024;
+					} else {
+						size = bitmap.getRowBytes() * bitmap.getHeight() / 1024;
+					}
+					return size;
+				}
+			};
 		}
 
+		@SuppressLint("NewApi")
 		@Override
 		public View getView(int position, View convertView, ViewGroup parent) {
 			ViewHolder viewHolder;
@@ -76,37 +104,56 @@ public class GoogleCardsActivity extends Activity {
 				viewHolder.textView = (TextView) view.findViewById(R.id.activity_googlecards_card_textview);
 				view.setTag(viewHolder);
 
-				ImageView imageView = (ImageView) view.findViewById(R.id.activity_googlecards_card_imageview);
-
-				int imageResId;
-				switch (getItem(position) % 5) {
-					case 0:
-						imageResId = R.drawable.img_nature1;
-						break;
-					case 1:
-						imageResId = R.drawable.img_nature2;
-						break;
-					case 2:
-						imageResId = R.drawable.img_nature3;
-						break;
-					case 3:
-						imageResId = R.drawable.img_nature4;
-						break;
-					default:
-						imageResId = R.drawable.img_nature5;
-				}
-				imageView.setImageResource(imageResId);
+				viewHolder.imageView = (ImageView) view.findViewById(R.id.activity_googlecards_card_imageview);
 			} else {
 				viewHolder = (ViewHolder) view.getTag();
 			}
 
 			viewHolder.textView.setText("This is card " + (getItem(position) + 1));
 
+			int imageResId;
+			switch (getItem(position) % 5) {
+				case 0:
+					imageResId = R.drawable.img_nature1;
+					break;
+				case 1:
+					imageResId = R.drawable.img_nature2;
+					break;
+				case 2:
+					imageResId = R.drawable.img_nature3;
+					break;
+				case 3:
+					imageResId = R.drawable.img_nature4;
+					break;
+				default:
+					imageResId = R.drawable.img_nature5;
+			}
+			Bitmap bitmap = getBitmapFromMemCache(imageResId);
+			if (bitmap == null) {
+				bitmap = BitmapFactory.decodeResource(mContext.getResources(), imageResId);
+				System.out.println("CREATING: " + (bitmap.getByteCount() / 1024));
+				addBitmapToMemoryCache(imageResId, bitmap);
+			} else {
+				System.out.println("REUSING");
+			}
+			viewHolder.imageView.setImageResource(imageResId);
+
 			return view;
+		}
+
+		private void addBitmapToMemoryCache(int key, Bitmap bitmap) {
+			if (getBitmapFromMemCache(key) == null) {
+				mMemoryCache.put(key, bitmap);
+			}
+		}
+
+		private Bitmap getBitmapFromMemCache(int key) {
+			return mMemoryCache.get(key);
 		}
 
 		private static class ViewHolder {
 			TextView textView;
+			ImageView imageView;
 		}
 	}
 }
