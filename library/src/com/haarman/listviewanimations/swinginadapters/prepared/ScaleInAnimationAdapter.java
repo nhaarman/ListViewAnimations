73cc3e4998fe2de0a7c98d58836c44b74550b415
Niek Haarman <haarman.niek@gmail.com>
2013-02-28 00:02:04 +0100

    Added parameters to ScaleInAnimationAdapter

--- library/src/com/haarman/listviewanimations/swinginadapters/prepared/ScaleInAnimationAdapter.java
+++ library/src/com/haarman/listviewanimations/swinginadapters/prepared/ScaleInAnimationAdapter.java
@@ -25,24 +25,41 @@ import com.nineoldandroids.animation.ObjectAnimator;
 
 public class ScaleInAnimationAdapter extends AnimationAdapter {
 
+	private static final float DEFAULTSCALEFROM = 0.8f;
+
+	private float mScaleFrom;
+	private long mAnimationDelayMillis;
+	private long mAnimationDurationMillis;
+
 	public ScaleInAnimationAdapter(BaseAdapter baseAdapter) {
+		this(baseAdapter, DEFAULTSCALEFROM);
+	}
+
+	public ScaleInAnimationAdapter(BaseAdapter baseAdapter, float scaleFrom) {
+		this(baseAdapter, scaleFrom, DEFAULTANIMATIONDELAYMILLIS, DEFAULTANIMATIONDURATIONMILLIS);
+	}
+
+	public ScaleInAnimationAdapter(BaseAdapter baseAdapter, float scaleFrom, long animationDelayMillis, long animationDurationMillis) {
 		super(baseAdapter);
+		mScaleFrom = scaleFrom;
+		mAnimationDelayMillis = animationDelayMillis;
+		mAnimationDurationMillis = animationDurationMillis;
 	}
 
 	@Override
 	protected long getAnimationDelayMillis() {
-		return DEFAULTANIMATIONDELAYMILLIS;
+		return mAnimationDelayMillis;
 	}
 
 	@Override
 	protected long getAnimationDurationMillis() {
-		return DEFAULTANIMATIONDURATIONMILLIS;
+		return mAnimationDurationMillis;
 	}
 
 	@Override
 	public Animator[] getAnimators(ViewGroup parent, View view) {
-		ObjectAnimator scaleX = ObjectAnimator.ofFloat(view, "scaleX", 0.8f, 1f);
-		ObjectAnimator scaleY = ObjectAnimator.ofFloat(view, "scaleY", 0.8f, 1f);
+		ObjectAnimator scaleX = ObjectAnimator.ofFloat(view, "scaleX", mScaleFrom, 1f);
+		ObjectAnimator scaleY = ObjectAnimator.ofFloat(view, "scaleY", mScaleFrom, 1f);
 		return new ObjectAnimator[] { scaleX, scaleY };
 	}
 
