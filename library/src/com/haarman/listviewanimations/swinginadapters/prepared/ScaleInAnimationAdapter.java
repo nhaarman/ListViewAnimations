26afb6a437762c703c850b45298527af9eb020c4
Niek Haarman <haarman.niek@gmail.com>
2013-02-27 23:55:10 +0100

    Added ScaleInAnimationAdapter

---
+++ library/src/com/haarman/listviewanimations/swinginadapters/prepared/ScaleInAnimationAdapter.java
@@ -0,0 +1,49 @@
+/*
+ * Copyright 2013 Niek Haarman
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.haarman.listviewanimations.swinginadapters.prepared;
+
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+
+import com.haarman.listviewanimations.swinginadapters.AnimationAdapter;
+import com.nineoldandroids.animation.Animator;
+import com.nineoldandroids.animation.ObjectAnimator;
+
+public class ScaleInAnimationAdapter extends AnimationAdapter {
+
+	public ScaleInAnimationAdapter(BaseAdapter baseAdapter) {
+		super(baseAdapter);
+	}
+
+	@Override
+	protected long getAnimationDelayMillis() {
+		return DEFAULTANIMATIONDELAYMILLIS;
+	}
+
+	@Override
+	protected long getAnimationDurationMillis() {
+		return DEFAULTANIMATIONDURATIONMILLIS;
+	}
+
+	@Override
+	public Animator[] getAnimators(ViewGroup parent, View view) {
+		ObjectAnimator scaleX = ObjectAnimator.ofFloat(view, "scaleX", 0.8f, 1f);
+		ObjectAnimator scaleY = ObjectAnimator.ofFloat(view, "scaleY", 0.8f, 1f);
+		return new ObjectAnimator[] { scaleX, scaleY };
+	}
+
+}
