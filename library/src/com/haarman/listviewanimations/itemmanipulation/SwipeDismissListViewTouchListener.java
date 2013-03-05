f682b4719bf65f7b01ef76c3e1ff5e0cc49915eb
Niek Haarman <haarman.niek@gmail.com>
2013-03-05 19:39:32 +0100

    Fixed license

--- library/src/com/haarman/listviewanimations/itemmanipulation/SwipeDismissListViewTouchListener.java
+++ library/src/com/haarman/listviewanimations/itemmanipulation/SwipeDismissListViewTouchListener.java
@@ -1,11 +1,13 @@
+// THIS IS A BETA! I DON'T RECOMMEND USING IT IN PRODUCTION CODE JUST YET
+ 
 /*
- * Copyright 2013 Niek Haarman
+ * Copyright 2012 Roman Nurik
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
- * http://www.apache.org/licenses/LICENSE-2.0
+ *     http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
