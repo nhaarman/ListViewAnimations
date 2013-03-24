fa851d172d5deb1e2a37035710c98b03abfd719c
Niek Haarman <haarman.niek@gmail.com>
2013-03-24 19:43:37 +0100

    Added modifiers

--- library/src/com/haarman/listviewanimations/itemmanipulation/contextualundo/ContextualUndoView.java
+++ library/src/com/haarman/listviewanimations/itemmanipulation/contextualundo/ContextualUndoView.java
@@ -16,55 +16,57 @@
  */
 package com.haarman.listviewanimations.itemmanipulation.contextualundo;
 
+import android.annotation.SuppressLint;
 import android.content.Context;
 import android.view.View;
 import android.widget.FrameLayout;
 
-class ContextualUndoView extends FrameLayout {
+@SuppressLint("ViewConstructor")
+public class ContextualUndoView extends FrameLayout {
 
 	private View undoView;
 	private View contentView;
 	private long itemId;
 
-	ContextualUndoView(Context context, int undoLayoutResourceId) {
+	public ContextualUndoView(Context context, int undoLayoutResourceId) {
 		super(context);
 		initUndo(undoLayoutResourceId);
 	}
 
-	void initUndo(int undoLayoutResourceId) {
+	private void initUndo(int undoLayoutResourceId) {
 		undoView = View.inflate(getContext(), undoLayoutResourceId, null);
 		addView(undoView);
 	}
 
-	void updateContentView(View contentView) {
+	public void updateContentView(View contentView) {
 		if (this.contentView == null) {
 			addView(contentView);
 		}
 		this.contentView = contentView;
 	}
 
-	View getContentView() {
+	public View getContentView() {
 		return contentView;
 	}
 
-	void setItemId(long itemId) {
+	public void setItemId(long itemId) {
 		this.itemId = itemId;
 	}
 
-	long getItemId() {
+	public long getItemId() {
 		return itemId;
 	}
 
-	boolean isContentDisplayed() {
+	public boolean isContentDisplayed() {
 		return contentView.getVisibility() == View.VISIBLE;
 	}
 
-	void displayUndo() {
+	public void displayUndo() {
 		contentView.setVisibility(View.GONE);
 		undoView.setVisibility(View.VISIBLE);
 	}
 
-	void displayContentView() {
+	public void displayContentView() {
 		contentView.setVisibility(View.VISIBLE);
 		undoView.setVisibility(View.GONE);
 	}
