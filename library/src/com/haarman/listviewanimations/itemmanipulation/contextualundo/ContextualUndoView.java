c92c47d4a934babb98f6aade94a404d30b2a37d3
Niek Haarman <haarman.niek@gmail.com>
2013-03-24 19:35:56 +0100

    Added licensing

--- library/src/com/haarman/listviewanimations/itemmanipulation/contextualundo/ContextualUndoView.java
+++ library/src/com/haarman/listviewanimations/itemmanipulation/contextualundo/ContextualUndoView.java
@@ -1,3 +1,19 @@
+/*
+ * Copyright 2013 Frankie Sardo
+ * Copyright 2013 Niek Haarman
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package com.haarman.listviewanimations.itemmanipulation.contextualundo;
 
 import android.content.Context;
@@ -6,54 +22,54 @@ import android.widget.FrameLayout;
 
 class ContextualUndoView extends FrameLayout {
 
-    private View undoView;
-    private View contentView;
-    private long itemId;
+	private View undoView;
+	private View contentView;
+	private long itemId;
 
-    ContextualUndoView(Context context, int undoLayoutResourceId) {
-        super(context);
-        initUndo(undoLayoutResourceId);
-    }
+	ContextualUndoView(Context context, int undoLayoutResourceId) {
+		super(context);
+		initUndo(undoLayoutResourceId);
+	}
 
-    void initUndo(int undoLayoutResourceId) {
-        undoView = View.inflate(getContext(), undoLayoutResourceId, null);
-        addView(undoView);
-    }
+	void initUndo(int undoLayoutResourceId) {
+		undoView = View.inflate(getContext(), undoLayoutResourceId, null);
+		addView(undoView);
+	}
 
-    static ContextualUndoView from(View view) {
-        return (ContextualUndoView) view;
-    }
+	static ContextualUndoView from(View view) {
+		return (ContextualUndoView) view;
+	}
 
-    void updateContentView(View contentView) {
-        if (this.contentView == null) {
-            addView(contentView);
-        }
-        this.contentView = contentView;
-    }
+	void updateContentView(View contentView) {
+		if (this.contentView == null) {
+			addView(contentView);
+		}
+		this.contentView = contentView;
+	}
 
-    View getContentView() {
-        return contentView;
-    }
+	View getContentView() {
+		return contentView;
+	}
 
-    void setItemId(long itemId) {
-        this.itemId = itemId;
-    }
+	void setItemId(long itemId) {
+		this.itemId = itemId;
+	}
 
-    long getItemId() {
-        return itemId;
-    }
+	long getItemId() {
+		return itemId;
+	}
 
-    boolean isContentDisplayed() {
-        return contentView.getVisibility() == View.VISIBLE;
-    }
+	boolean isContentDisplayed() {
+		return contentView.getVisibility() == View.VISIBLE;
+	}
 
-    void displayUndo() {
-        contentView.setVisibility(View.GONE);
-        undoView.setVisibility(View.VISIBLE);
-    }
+	void displayUndo() {
+		contentView.setVisibility(View.GONE);
+		undoView.setVisibility(View.VISIBLE);
+	}
 
-    void displayContentView() {
-        contentView.setVisibility(View.VISIBLE);
-        undoView.setVisibility(View.GONE);
-    }
+	void displayContentView() {
+		contentView.setVisibility(View.VISIBLE);
+		undoView.setVisibility(View.GONE);
+	}
 }
