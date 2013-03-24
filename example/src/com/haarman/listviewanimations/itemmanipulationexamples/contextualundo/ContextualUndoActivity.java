51c7cb54401ff727e288654c390e630869ffdf33
Niek Haarman <haarman.niek@gmail.com>
2013-03-24 22:35:21 +0100

    Recreated ContextualUndoActivity

--- example/src/com/haarman/listviewanimations/itemmanipulationexamples/contextualundo/ContextualUndoActivity.java
+++ example/src/com/haarman/listviewanimations/itemmanipulationexamples/contextualundo/ContextualUndoActivity.java
@@ -1,92 +1,33 @@
 package com.haarman.listviewanimations.itemmanipulationexamples.contextualundo;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-
-import android.content.Context;
 import android.os.Bundle;
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
-import android.widget.TextView;
 
 import com.haarman.listviewanimations.ArrayAdapter;
 import com.haarman.listviewanimations.MyListActivity;
 import com.haarman.listviewanimations.R;
 import com.haarman.listviewanimations.itemmanipulation.contextualundo.ContextualUndoAdapter;
+import com.haarman.listviewanimations.itemmanipulation.contextualundo.ContextualUndoAdapter.DeleteItemCallback;
 
 public class ContextualUndoActivity extends MyListActivity {
 
-    private ContextualUndoAdapter contextualUndoAdapter;
+	private final ArrayAdapter<String> mAdapter = createListAdapter();
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
 
-        final ArrayAdapter<String> mAdapter = createListAdapter();
-        contextualUndoAdapter = new ContextualUndoAdapter(mAdapter, R.layout.undo_row, R.id.undo);
+		ContextualUndoAdapter contextualUndoAdapter = new ContextualUndoAdapter(mAdapter, R.layout.undo_row, R.id.undo);
+		contextualUndoAdapter.setListView(getListView());
+		getListView().setAdapter(contextualUndoAdapter);
+		contextualUndoAdapter.setDeleteItemCallback(new MyDeleteItemCallback());
+	}
 
-        if (savedInstanceState != null) {
-            contextualUndoAdapter.onRestoreInstanceState(savedInstanceState.getParcelable("contextualUndoAdapterState"));
-        }
-        contextualUndoAdapter.setListView(getListView());
-        getListView().setAdapter(contextualUndoAdapter);
-        contextualUndoAdapter.setDeleteItemCallback(new ContextualUndoAdapter.DeleteItemCallback() {
-            @Override
-            public void deleteItem(Object item) {
-                performActualDelete(item);
-                mAdapter.remove((String) item);
-                mAdapter.notifyDataSetChanged();
-            }
-        });
+	private class MyDeleteItemCallback implements DeleteItemCallback {
 
-        getListView().setOnItemClickListener(null);
-    }
-
-    private void performActualDelete(Object item) {
-        ITEMS.remove(item);
-    }
-
-    @Override
-    protected void onSaveInstanceState(Bundle outState) {
-        super.onSaveInstanceState(outState);
-        outState.putParcelable("contextualUndoAdapterState", contextualUndoAdapter.onSaveInstanceState());
-    }
-
-    @Override
-    protected ArrayAdapter<String> createListAdapter() {
-        return new MyListAdapter(this, ITEMS);
-    }
-
-    private static final ArrayList<String> ITEMS = new ArrayList<String>();
-
-    static {
-        String[] words = "Remember: in order to use the Contextual Undo, your decorated adapter must have sable ids.".split(" ");
-        ITEMS.addAll(Arrays.asList(words));
-    }
-
-    private static class MyListAdapter extends ArrayAdapter<String> {
-
-        private Context mContext;
-
-        public MyListAdapter(Context context, ArrayList<String> items) {
-            super(items);
-            mContext = context;
-        }
-
-        @Override
-        public long getItemId(int position) {
-            return getItem(position).hashCode();
-        }
-
-        @Override
-        public View getView(int position, View convertView, ViewGroup parent) {
-            TextView tv = (TextView) convertView;
-            if (tv == null) {
-                tv = (TextView) LayoutInflater.from(mContext).inflate(R.layout.list_row, parent, false);
-            }
-            tv.setText(getItem(position));
-            return tv;
-        }
-    }
+		@Override
+		public void deleteItem(Object item) {
+			mAdapter.remove((String) item);
+			mAdapter.notifyDataSetChanged();
+		}
+	}
 }
