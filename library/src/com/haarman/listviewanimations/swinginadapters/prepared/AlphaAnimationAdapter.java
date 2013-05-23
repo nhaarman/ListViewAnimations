240b2a77be2a9baf9b04024c85feb9aac6230e40
Niek Haarman <niek@label305.com>
2013-05-23 15:44:54 +0200

    Added AlphaAnimationAdapter

---
+++ library/src/com/haarman/listviewanimations/swinginadapters/prepared/AlphaAnimationAdapter.java
@@ -0,0 +1,30 @@
+package com.haarman.listviewanimations.swinginadapters.prepared;
+
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+
+import com.haarman.listviewanimations.swinginadapters.AnimationAdapter;
+import com.nineoldandroids.animation.Animator;
+
+public class AlphaAnimationAdapter extends AnimationAdapter {
+
+	public AlphaAnimationAdapter(BaseAdapter baseAdapter) {
+		super(baseAdapter);
+	}
+
+	@Override
+	protected long getAnimationDelayMillis() {
+		return DEFAULTANIMATIONDELAYMILLIS;
+	}
+
+	@Override
+	protected long getAnimationDurationMillis() {
+		return DEFAULTANIMATIONDURATIONMILLIS;
+	}
+
+	@Override
+	public Animator[] getAnimators(ViewGroup parent, View view) {
+		return new Animator[0];
+	}
+}
