e6fb4828ba80399a84effaa9d682ad1a7cffbc92
Niek Haarman <haarman.niek@gmail.com>
2013-12-08 17:32:49 +0100

    Added methods for programmatically expand / collapse items: expand(int), collapse(int), toggle(int). Fixes #77.

--- example/src/com/haarman/listviewanimations/itemmanipulationexamples/ExpandableListItemActivity.java
+++ example/src/com/haarman/listviewanimations/itemmanipulationexamples/ExpandableListItemActivity.java
@@ -1,7 +1,5 @@
 package com.haarman.listviewanimations.itemmanipulationexamples;
 
-import java.util.List;
-
 import android.content.Context;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
@@ -20,123 +18,125 @@ import com.haarman.listviewanimations.R;
 import com.haarman.listviewanimations.itemmanipulation.ExpandableListItemAdapter;
 import com.haarman.listviewanimations.swinginadapters.prepared.AlphaInAnimationAdapter;
 
+import java.util.List;
+
 public class ExpandableListItemActivity extends MyListActivity {
 
-	private MyExpandableListItemAdapter mExpandableListItemAdapter;
-	private boolean mLimited;
+    private MyExpandableListItemAdapter mExpandableListItemAdapter;
+    private boolean mLimited;
 
-	@Override
-	protected void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
 
-		mExpandableListItemAdapter = new MyExpandableListItemAdapter(this, getItems());
-		AlphaInAnimationAdapter alphaInAnimationAdapter = new AlphaInAnimationAdapter(mExpandableListItemAdapter);
-		alphaInAnimationAdapter.setAbsListView(getListView());
-		alphaInAnimationAdapter.setInitialDelayMillis(500);
-		getListView().setAdapter(alphaInAnimationAdapter);
+        mExpandableListItemAdapter = new MyExpandableListItemAdapter(this, getItems());
+        AlphaInAnimationAdapter alphaInAnimationAdapter = new AlphaInAnimationAdapter(mExpandableListItemAdapter);
+        alphaInAnimationAdapter.setAbsListView(getListView());
+        alphaInAnimationAdapter.setInitialDelayMillis(500);
+        getListView().setAdapter(alphaInAnimationAdapter);
 
-		Toast.makeText(this, R.string.explainexpand, Toast.LENGTH_LONG).show();
-	}
+        Toast.makeText(this, R.string.explainexpand, Toast.LENGTH_LONG).show();
+    }
 
-	private static class MyExpandableListItemAdapter extends ExpandableListItemAdapter<Integer> {
+    private static class MyExpandableListItemAdapter extends ExpandableListItemAdapter<Integer> {
 
-		private Context mContext;
-		private LruCache<Integer, Bitmap> mMemoryCache;
+        private Context mContext;
+        private LruCache<Integer, Bitmap> mMemoryCache;
 
-		/**
-		 * Creates a new ExpandableListItemAdapter with the specified list, or an empty list if
-		 * items == null.
-		 */
-		private MyExpandableListItemAdapter(Context context, List<Integer> items) {
-			super(context, R.layout.activity_expandablelistitem_card, R.id.activity_expandablelistitem_card_title, R.id.activity_expandablelistitem_card_content, items);
-			mContext = context;
+        /**
+         * Creates a new ExpandableListItemAdapter with the specified list, or an empty list if
+         * items == null.
+         */
+        private MyExpandableListItemAdapter(Context context, List<Integer> items) {
+            super(context, R.layout.activity_expandablelistitem_card, R.id.activity_expandablelistitem_card_title, R.id.activity_expandablelistitem_card_content, items);
+            mContext = context;
 
-			final int maxMemory = (int) (Runtime.getRuntime().maxMemory() / 1024);
+            final int maxMemory = (int) (Runtime.getRuntime().maxMemory() / 1024);
 
-			// Use 1/8th of the available memory for this memory cache.
-			final int cacheSize = maxMemory;
-			mMemoryCache = new LruCache<Integer, Bitmap>(cacheSize) {
-				@Override
-				protected int sizeOf(Integer key, Bitmap bitmap) {
-					// The cache size will be measured in kilobytes rather than
-					// number of items.
-					return bitmap.getRowBytes() * bitmap.getHeight() / 1024;
-				}
-			};
-		}
+            // Use 1/8th of the available memory for this memory cache.
+            final int cacheSize = maxMemory;
+            mMemoryCache = new LruCache<Integer, Bitmap>(cacheSize) {
+                @Override
+                protected int sizeOf(Integer key, Bitmap bitmap) {
+                    // The cache size will be measured in kilobytes rather than
+                    // number of items.
+                    return bitmap.getRowBytes() * bitmap.getHeight() / 1024;
+                }
+            };
+        }
 
-		@Override
-		public View getTitleView(int position, View convertView, ViewGroup parent) {
-			TextView tv = (TextView) convertView;
-			if (tv == null) {
-				tv = new TextView(mContext);
-			}
-			tv.setText(mContext.getString(R.string.expandorcollapsecard, getItem(position)));
-			return tv;
-		}
+        @Override
+        public View getTitleView(int position, View convertView, ViewGroup parent) {
+            TextView tv = (TextView) convertView;
+            if (tv == null) {
+                tv = new TextView(mContext);
+            }
+            tv.setText(mContext.getString(R.string.expandorcollapsecard, getItem(position)));
+            return tv;
+        }
 
-		@Override
-		public View getContentView(int position, View convertView, ViewGroup parent) {
-			ImageView imageView = (ImageView) convertView;
-			if (imageView == null) {
-				imageView = new ImageView(mContext);
-				imageView.setScaleType(ImageView.ScaleType.CENTER_CROP);
-			}
+        @Override
+        public View getContentView(int position, View convertView, ViewGroup parent) {
+            ImageView imageView = (ImageView) convertView;
+            if (imageView == null) {
+                imageView = new ImageView(mContext);
+                imageView.setScaleType(ImageView.ScaleType.CENTER_CROP);
+            }
 
-			int imageResId;
-			switch (getItem(position) % 5) {
-			case 0:
-				imageResId = R.drawable.img_nature1;
-				break;
-			case 1:
-				imageResId = R.drawable.img_nature2;
-				break;
-			case 2:
-				imageResId = R.drawable.img_nature3;
-				break;
-			case 3:
-				imageResId = R.drawable.img_nature4;
-				break;
-			default:
-				imageResId = R.drawable.img_nature5;
-			}
+            int imageResId;
+            switch (getItem(position) % 5) {
+                case 0:
+                    imageResId = R.drawable.img_nature1;
+                    break;
+                case 1:
+                    imageResId = R.drawable.img_nature2;
+                    break;
+                case 2:
+                    imageResId = R.drawable.img_nature3;
+                    break;
+                case 3:
+                    imageResId = R.drawable.img_nature4;
+                    break;
+                default:
+                    imageResId = R.drawable.img_nature5;
+            }
 
-			Bitmap bitmap = getBitmapFromMemCache(imageResId);
-			if (bitmap == null) {
-				bitmap = BitmapFactory.decodeResource(mContext.getResources(), imageResId);
-				addBitmapToMemoryCache(imageResId, bitmap);
-			}
-			imageView.setImageBitmap(bitmap);
+            Bitmap bitmap = getBitmapFromMemCache(imageResId);
+            if (bitmap == null) {
+                bitmap = BitmapFactory.decodeResource(mContext.getResources(), imageResId);
+                addBitmapToMemoryCache(imageResId, bitmap);
+            }
+            imageView.setImageBitmap(bitmap);
 
-			return imageView;
-		}
+            return imageView;
+        }
 
-		private void addBitmapToMemoryCache(int key, Bitmap bitmap) {
-			if (getBitmapFromMemCache(key) == null) {
-				mMemoryCache.put(key, bitmap);
-			}
-		}
+        private void addBitmapToMemoryCache(int key, Bitmap bitmap) {
+            if (getBitmapFromMemCache(key) == null) {
+                mMemoryCache.put(key, bitmap);
+            }
+        }
 
-		private Bitmap getBitmapFromMemCache(int key) {
-			return mMemoryCache.get(key);
-		}
-	}
+        private Bitmap getBitmapFromMemCache(int key) {
+            return mMemoryCache.get(key);
+        }
+    }
 
-	@Override
-	public boolean onCreateOptionsMenu(Menu menu) {
-		getMenuInflater().inflate(R.menu.menu_expandablelistitem, menu);
-		return super.onCreateOptionsMenu(menu);
-	}
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_expandablelistitem, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
 
-	@Override
-	public boolean onOptionsItemSelected(MenuItem item) {
-		switch (item.getItemId()) {
-		case R.id.menu_expandable_limit:
-			mLimited = !mLimited;
-			item.setChecked(mLimited);
-			mExpandableListItemAdapter.setLimit(mLimited ? 2 : 0);
-			return true;
-		}
-		return super.onOptionsItemSelected(item);
-	}
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.menu_expandable_limit:
+                mLimited = !mLimited;
+                item.setChecked(mLimited);
+                mExpandableListItemAdapter.setLimit(mLimited ? 2 : 0);
+                return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
 }
