667841892ae392e83f77e265c26339fd6f034f5f
fenemoj <joshgfenemore@gmail.com>
2014-02-27 14:07:39 +1300

    Update ContextualUndoView.java
    Change so when content view is replaced by contextual dismiss view, the list view item wraps to the size of the contextual dismiss view.
    Change made for when combining contextual undo adapter with expandable list view adapter.

--- library/src/com/nhaarman/listviewanimations/itemmanipulation/swipedismiss/contextualundo/ContextualUndoView.java
+++ library/src/com/nhaarman/listviewanimations/itemmanipulation/swipedismiss/contextualundo/ContextualUndoView.java
@@ -76,12 +76,12 @@ public class ContextualUndoView extends FrameLayout {
 
     public void displayUndo() {
         updateCountDownTimer("");
-        mContentView.setVisibility(View.INVISIBLE);
+        mContentView.setVisibility(View.GONE);
         mUndoView.setVisibility(View.VISIBLE);
     }
 
     public void displayContentView() {
         mContentView.setVisibility(View.VISIBLE);
-        mUndoView.setVisibility(View.INVISIBLE);
+        mUndoView.setVisibility(View.GONE);
     }
 }
