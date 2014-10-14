b46793544fd9022c530717eed52039c6279b3710
Martin Guillon <martin.guillon@akylas.fr>
2014-10-14 16:13:04 +0200

    cleanup

--- lib-manipulation/src/main/java/com/nhaarman/listviewanimations/itemmanipulation/DynamicListView.java
+++ lib-manipulation/src/main/java/com/nhaarman/listviewanimations/itemmanipulation/DynamicListView.java
@@ -19,7 +19,6 @@ package com.nhaarman.listviewanimations.itemmanipulation;
 import android.content.Context;
 import android.content.res.Resources;
 import android.graphics.Canvas;
-import android.os.Build;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.util.AttributeSet;
